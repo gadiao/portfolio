ee770a9cd64540d851c42dfa32f568706ea666ed
Gil <giladiao@hotmail.com>
2023-07-11 01:16:22 -0400

    edited theme.js

--- src/theme.js
+++ src/theme.js
@@ -2,20 +2,6 @@ import { createTheme } from '@mui/material/styles';
 import data from "../data.json";
 const { theme } = data
 
-export const lightTheme = createTheme(
-  (theme && theme.light && Object.keys(theme.light).length > 0) ? theme.light :
-  {
-    palette: {
-      mode: "light"
-    }
-  }
-)
+export const lightTheme = createTheme(theme.light)
 
-export const darkTheme = createTheme(
-  (theme && theme.dark && Object.keys(theme.dark).length > 0) ? theme.dark :
-  {
-    palette: {
-      mode: "dark"
-    }
-  }
-)
+export const darkTheme = createTheme(theme.dark)
