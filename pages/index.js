7c90119991ed31830cc993652f2914be8a9c2d35
Gil <giladiao@hotmail.com>
2023-07-11 16:32:02 -0400

    edited index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React, { useCallback } from 'react';
 import { Box, AppBar, Container, IconButton, Toolbar, Typography, useScrollTrigger } from '@mui/material';
-import { useTheme } from '@mui/material/styles';
+import { useTheme, ThemeProvider } from '@mui/material/styles';
 import Landing from '../src/Landing';
 import Projects from '../src/Projects';
 import Skills from '../src/Skills';
@@ -22,24 +22,26 @@ export default function Index({ setTheme }) {
   }, [setTheme])
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar color={!trigger ? "transparent" : "inherit"} sx={{ boxShadow: "none" }} position= "fixed">
-        <Toolbar>
-          <Typography variant="h6" sx={{ flexGrow: 1 }}>
-            { name }
-          </Typography>
-          <IconButton edge="end" color="inherit" onClick={toggleTheme}>
-            {theme.palette.mode === "dark" ? <Brightness7/> : <Brightness4/>}
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-      <Container>
-        <Landing />
-        <Projects />
-        <Skills />
-        <Experience />
-        <About />
-      </Container>
-    </Box>
+    <ThemeProvider theme={theme}>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar color={!trigger ? "transparent" : "inherit"} sx={{ boxShadow: "none" }} position= "fixed">
+          <Toolbar>
+            <Typography variant="h6" sx={{ flexGrow: 1 }}>
+              { name }
+            </Typography>
+            <IconButton edge="end" color="inherit" onClick={toggleTheme}>
+              {theme.palette.mode === "dark" ? <Brightness7/> : <Brightness4/>}
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Container>
+          <Landing />
+          <Projects />
+          <Skills />
+          <Experience />
+          <About />
+        </Container>
+      </Box>
+    </ThemeProvider>
   );
 }
