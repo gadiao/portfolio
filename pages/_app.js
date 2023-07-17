7666c707dc5e62fcb997af7c4cd5be0d682f9ab0
Gil <giladiao@hotmail.com>
2023-07-17 01:12:46 -0400

    edited _app.json and package.json

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
-import { useMediaQuery, CssBaseline } from '@mui/material';
+import { CssBaseline } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles'
 import { CacheProvider } from '@emotion/react';
-import { darkTheme, lightTheme } from '../src/theme';
+import { darkTheme } from '../src/theme';
 import createEmotionCache from '../src/createEmotionCache';
+import { Analytics } from '@vercel/analytics/react';
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -25,6 +26,7 @@ export default function MyApp(props) {
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         <Component {...pageProps} setTheme={setTheme}/>
+        <Analytics />
       </ThemeProvider>
     </CacheProvider>
   );
